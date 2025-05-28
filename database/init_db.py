d1a7c52e0cb953051f0c44da25f77ff6872731da
pratik1100101 <pratik.borude12@gmail.com>
2025-05-28 20:49:56 +0530

    Commented the init_db script.

--- database/init_db.py
+++ database/init_db.py
@@ -1,18 +1,24 @@
 from database.db import create_user, get_user_by_username
 from database.db_config import Base, SessionLocal, db
-import sqlalchemy.orm as so
 
 from utils.auth_utils import hash_password
 
 
 def init_db():
+
+    # Creates a database if it doesn't already exist.
+    # Seeds the database with an initial admin user.
+
     print("Attempting to initialize database tables...")
 
+    # IMPORTANT: Base.metadata.create_all() does NOT drop or recreate existing tables.
+    # It only creates tables if they are missing.
     Base.metadata.create_all(bind=db)
     print("Database tables checked/created.")
 
-    session: so.Session = SessionLocal()
+    session = SessionLocal()
     try:
+        # Checks if initial user exists else it will seed it.
         admin_user = get_user_by_username(session, "admin")
 
         if not admin_user:
@@ -23,10 +29,13 @@ def init_db():
         else:
             print("'admin' user already exists. Skipping creation.")
     except Exception as e:
+        # If any error occurs during seeding rollback the transaction to ensure data integrity.
         session.rollback()
         print(f"Error during database initialization or admin user creation: {e}")
         raise
     finally:
+        # Always close the database session to release the connection back to the pool.
+        # This is crucial to prevent resource leaks.
         session.close()
 
 
