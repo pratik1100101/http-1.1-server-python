ec0a3fc6c46b2cb247e2096c8772bff732308a25
pratik1100101 <pratik.borude12@gmail.com>
2025-05-29 13:04:04 +0530

    Removed unnecessary commented code and improved comments.

--- server.py
+++ server.py
@@ -15,7 +15,6 @@ load_dotenv()
 WEB_ROOT_DIR = os.getenv("WEB_ROOT_DIR")
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
-SECRECT_KEY = os.getenv("JWT_SECRET_KEY")
 
 
 def main():
@@ -24,11 +23,14 @@ def main():
         print(f"Could not start server as the host or port or web_root_dir is not set.")
         return
 
+    print("Initializing database...")
     init_db()
-    print("Database initialization complete for server startup.")
+    print("Database initialization complete.")
 
     # Initialize the Router.
     router = Router()
+    load_routes(router)
+    print("Routes loaded into router.")
 
     # Initialize the WebServer.
     server = WebServer(HOST, PORT, WEB_ROOT_DIR, router)
@@ -36,8 +38,7 @@ def main():
     server.add_middleware(logger_middleware)
     server.add_middleware(auth_middleware)
     # Load routes after middleware is added
-    # Add here if you are applying the programmatic routing logic and remove load_routes
-    load_routes(router)
+
     print(f"Starting server on http://{HOST}:{PORT}")
     try:
         # Start server once all checks are done
@@ -48,30 +49,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# Add this to the comment # Add here... if you are using programmatic routing
-##Define the routes using decorators.
-##### Static file routes
-####@router.route("GET", "/", filepath="index.html", web_root_dir=web_root_dir)
-####def index_route_handler(request, **kwargs):
-######### This wrapper handler's job is to call the actual serve_static_file handler.
-######### It receives `request` and unpacked `handler_args` from the decorator.
-########return serve_static_file(request, **kwargs)
-
-####@router.route("GET", "/style.css", filepath="style.css", web_root_dir=web_root_dir)
-####def style_route_handler(request, **kwargs):
-########return serve_static_file(request, **kwargs)
-
-####@router.route("GET", "/script.js", filepath="script.js", web_root_dir=web_root_dir)
-####def script_route_handler(request, **kwargs):
-########return serve_static_file(request, **kwargs)
-
-##### API routes
-####@router.route("GET", "/api/data")
-####def api_get_data_handler(request, **kwargs): # kwargs is present for consistency but not used here
-########return get_data(request)
-
-####@router.route("POST", "/api/data")
-####def api_post_data_handler(request, **kwargs): # kwargs is present for consistency but not used here
-########return post_data(request)
